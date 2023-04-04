b8a1c67fe5b3ac2d1f25230aea931ae251369192
pabloe1975 <128752977+pabloe1975@users.noreply.github.com>
2023-04-04 16:24:13 -0300

    Add files via upload

---
+++ Extras13.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia4a6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Extras13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese la altura de la escalera: ");
+        int altura = sc.nextInt();
+
+        for (int i = 1; i <= altura; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
