2e6522846fc22a77656876618f2b13110113b5e5
pabloe1975 <128752977+pabloe1975@users.noreply.github.com>
2023-04-04 16:20:07 -0300

    Add files via upload

---
+++ Extras4.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia4a6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Extras4 {
+
+    /**
+     * @param args the command line arguments
+     */
+        public static void main(String[] args) {
+                Scanner leer = new Scanner(System.in);
+                int opcion;
+                System.out.println(" Ingrese numero");
+opcion = leer.nextInt();
+switch(opcion){
+        case 1:
+        System.out.println(" El equivalente en numero romano es I");
+        break;
+        case 2:
+        System.out.println(" El equivalente en numero romano es II");
+        break;
+        case 3:
+        System.out.println(" El equivalente en numero romano es III");
+        break;
+        case 4:
+        System.out.println(" El equivalente en numero romano es IV");
+        break;
+        case 5:
+        System.out.println(" El equivalente en numero romano es V");
+        break;
+        case 6:
+        System.out.println(" El equivalente en numero romano es VI");
+        break;
+        case 7:
+        System.out.println(" El equivalente en numero romano es VII");
+        break;
+        case 8:
+        System.out.println(" El equivalente en numero romano es VIII");
+        break;
+        case 9:
+        System.out.println(" El equivalente en numero romano es IX");
+        break;
+        case 10:
+        System.out.println(" El equivalente en numero romano es X");
+        break;
+    }
+    }
+    
+}
